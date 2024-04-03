0d6effb241469510df5f0b34c1e06a3286c03ff0
analiciasosa2019 <analiciasosa2019@gmail.com>
2024-04-03 16:45:46 -0700

    changed add nav bar

---
+++ navigation.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navigationContainer = document.getElementById("navigation-container");
+    navigationContainer.innerHTML = `
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+    `;
+});
